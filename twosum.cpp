42f8bcafdb53203e646f84343df5d9617231115b
Siddique Ahmad <ahmadsiddique7073@gmail.com>
2023-06-04 15:28:22 +0530

    2nd commit

---
+++ twosum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+          
+          unordered_map<int,int> m;
+          for(int i=0;i<nums.size();i++){
+              int x=nums[i];
+              int y=target-x;
+              if(m.find(y)!=m.end()){
+                   return {i,m[y]}; 
+              }
+              m[nums[i]]=i;
+          }
+          return {-1,-1};
+          
+       
+    
+    }
+};
