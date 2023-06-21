eff7c87ab3549a2bbbc8acb0abbd1256623c65d4
Siddique Ahmad <ahmadsiddique7073@gmail.com>
2023-06-21 07:08:21 +0530

    valid_palindrome

---
+++ validpalindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int l=0;
+        int h=s.size()-1;
+        transform(s.begin(),s.end(),s.begin(), :: tolower);
+        while(l<=h){
+            if(!isalnum(s[l]))
+            l++;
+            else if(!isalnum(s[h]))
+            h--;
+            else if(s[l]!=s[h])
+            return 0;
+
+            else
+            {
+                l++;
+                h--;
+            }
+        }
+        return 1;
+    }
+};
