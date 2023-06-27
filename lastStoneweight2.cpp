5bf6dcd0215b28c8a8cb3e018e21c267e583eab3
Siddique Ahmad <ahmadsiddique7073@gmail.com>
2023-06-27 08:13:54 +0530

    somechanges

--- lastStoneweight2.cpp
+++ lastStoneweight2.cpp
@@ -15,10 +15,10 @@ public:
        /* int n=stones.size();
         int t[n+1][sum+1];
           
-          for(int i=0;i<n+1;i++)
+          for(int i=0;i<sum+1;i++)
           t[0][i]=0;
 
-          for(int i=0;i<sum+1;i++)
+          for(int i=0;i<n+1;i++)
           t[i][0]=1;
 
           for(int i=1;i<n+1;i++){
